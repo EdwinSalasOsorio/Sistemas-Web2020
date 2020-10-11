f88b870594e0bac9c21de64020a010919b5ebceb
Edwin Osorio <EdwinSalasOsorio>
2020-10-10 19:52:39 -0500

    Spark

---
+++ practica5/src/main/java/mx/uv/sw80640/App.java
@@ -0,0 +1,19 @@
+package mx.uv.sw80640;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        port(2020);
+        get("/", (req, res) -> "Hola desde 343 Guilty Spark");
+        get("/hola", (req, res) -> "Hola hola");
+        get("/adios", (req, res) -> "Adios desde 343 Guilty Spark");
+
+    }
+}
