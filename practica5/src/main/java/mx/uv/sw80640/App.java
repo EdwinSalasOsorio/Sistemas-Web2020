7e3a5b1a0b3f25c4c5337da7c539270675bf9a85
Edwin Osorio <EdwinSalasOsorio>
2020-10-15 19:11:31 -0500

    Backend

--- practica5/src/main/java/mx/uv/sw80640/App.java
+++ practica5/src/main/java/mx/uv/sw80640/App.java
@@ -10,10 +10,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        port(2020);
-        get("/", (req, res) -> "Hola desde 343 Guilty Spark");
-        get("/hola", (req, res) -> "Hola hola");
-        get("/adios", (req, res) -> "Adios desde 343 Guilty Spark");
+        port(80);
+        
+        options("/*", (request, response) -> {
 
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+
+
+        get("/", (request, response) -> "Hola desde 343 Guilty Spark");
+        get("/hola", (request, response) -> {
+            System.out.println("Request: " + request.queryParams());
+            System.out.println("Request: " + request.queryParams("PrmEmail"));
+            System.out.println("Request: " + request.queryParams("PrmPassword"));
+            return "Hola " + request.queryParams("PrmEmail") + " desde 343 Guilty Spark";
+        });
+
+        post("/adios", (request, response) -> {
+            System.out.println("Request: " + request.queryParams());
+            System.out.println("Request: " + request.queryParams("PrmEmail"));
+            System.out.println("Request: " + request.queryParams("PrmPassword"));
+            System.out.println("Request: " + request.body()); // pendiente ver como llenar el body
+            System.out.println("Request: " + request.contentType());
+            return "Hola " + request.queryParams("PrmEmail") + " desde 343 Guilty Spark";
+        });
     }
 }
